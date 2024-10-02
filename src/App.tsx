181b5a0703b5e54962e48dc798485ee6a4354d34
Seymur Orucov <seymur.o@bestcomp.net>
2024-10-02 15:59:18 +0400

    change dir

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import { UsersProvider } from "./users/components/UsersProvider.tsx";
-
-export function App() {
-  return <UsersProvider />;
-}
--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const patterns = {
-  email: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
-};
