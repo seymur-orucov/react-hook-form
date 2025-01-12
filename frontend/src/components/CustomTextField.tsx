0db9dcf85296f893f7bdd9f6df2883dbe4dca0ae
seymur-orucov <seymur92@hotmail.com>
2025-01-13 00:26:32 +0400

    done

--- frontend/src/components/CustomTextField.tsx
+++ frontend/src/components/CustomTextField.tsx
@@ -1,46 +1,44 @@
-import { FieldValues, Path, useFormContext } from "react-hook-form";
+import { Control, FieldValues, Path } from "react-hook-form";
 
 import { FormField, FormMessage } from "./core/form";
 import { FormItem, FormLabel, FormControl } from "@/components/core/form";
 import { Input } from "./core/input";
 
-interface Props<T extends FieldValues> {
+type Props<T extends FieldValues> = {
   name: Path<T>;
   label: string;
-}
+  control?: Control<T>;
+};
 
 export function CustomTextField<T extends FieldValues>({
   name,
   label,
+  control,
 }: Props<T>) {
-  const { control } = useFormContext<T>();
+  if (control) {
+    return (
+      <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{label}</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    );
+  }
 
   return (
-    <FormField
-      control={control}
-      name={name}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>{label}</FormLabel>
-          <FormControl>
-            <Input placeholder="shadcn" type="" {...field} />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-
-    // <Controller
-    //   control={control}
-    //   name={name}
-    //   render={({ field, fieldState: { error } }) => (
-    //     <TextField
-    //       {...field}
-    //       {...props}
-    //       error={!!error}
-    //       helperText={error?.message}
-    //     />
-    //   )}
-    // />
+    <FormItem>
+      <FormLabel>{label}</FormLabel>
+      <FormControl>
+        <Input placeholder="shadcn" name={name} />
+      </FormControl>
+    </FormItem>
   );
 }
